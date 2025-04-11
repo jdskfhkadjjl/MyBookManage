1969404c390202ed3640ecfd580d7a92412ce6b4
MiHu <2150569205@qq.com>
2025-04-11 19:10:01 +0800

    图书管理系统重构版本1.0.0-添加了knife4j的配置

--- src/main/java/com/bookmanage/SpringBootApplications.java
+++ src/main/java/com/bookmanage/SpringBootApplications.java
@@ -2,8 +2,10 @@ package com.bookmanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("mapper")
 public class SpringBootApplications {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplications.class,args);
